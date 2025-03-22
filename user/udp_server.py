e39b99a7768c21ff25a4672239ff8e52d66a37d7
JIYIC-P <1467612622@qq.com>
2025-03-22 17:20:00 +0800

    Add files via upload

---
+++ user/udp_server.py
@@ -0,0 +1,29 @@
+import asyncio
+from user.usr_shortcut_handler import udp_mode
+from util.client_cosmic import Cosmic
+
+class EchoServerProtocol(asyncio.DatagramProtocol):
+    
+    def connection_made(self, transport):
+        self.transport = transport
+    def datagram_received(self, data, addr):
+        message = data.decode()
+        print(f"收到来自 {addr} 的消息: {message}")
+        udp_mode(message)
+
+async def start():
+    transport, protocol = await Cosmic.loop.create_datagram_endpoint(
+        lambda: EchoServerProtocol(),
+        local_addr=('127.0.0.1', 9999)
+    )
+    print("UDP服务器启动，监听端口9999...")
+
+
+async def main():
+    await start()
+    while True:
+        await asyncio.sleep(1000)
+
+if __name__ == "__main__":
+    Cosmic.loop = asyncio.get_event_loop()
+    Cosmic.loop.run_until_complete(main())
---
+++ user/usr_shortcut_handler.py
@@ -0,0 +1,154 @@
+"""from util.client_cosmic import Cosmic, console
+from config import ClientConfig as Config
+
+import time
+import asyncio
+from threading import Event
+from concurrent.futures import ThreadPoolExecutor
+from util.client_send_audio import send_audio
+from util.my_status import Status
+
+
+task = asyncio.Future()
+status = Status('开始录音', spinner='point')
+pool = ThreadPoolExecutor()
+pressed = False
+released = True
+event = Event()
+
+
+def launch_task():
+    global task
+
+    # 记录开始时间
+    t1 = time.time()
+
+    # 将开始标志放入队列
+    asyncio.run_coroutine_threadsafe(
+        Cosmic.queue_in.put({'type': 'begin', 'time': t1, 'data': None}),
+        Cosmic.loop
+    )
+
+    # 通知录音线程可以向队列放数据了
+    Cosmic.on = t1
+
+    # 打印动画：正在录音
+    status.start()
+
+    # 启动识别任务
+    task = asyncio.run_coroutine_threadsafe(
+        send_audio(),
+        Cosmic.loop,
+    )
+
+
+def cancel_task():
+    # 通知停止录音，关掉滚动条
+    Cosmic.on = False
+    status.stop()
+
+    # 取消协程任务
+    task.cancel()
+
+
+def finish_task():
+    global task
+
+    # 通知停止录音，关掉滚动条
+    Cosmic.on = False
+    status.stop()
+
+    # 通知结束任务
+    asyncio.run_coroutine_threadsafe(
+        Cosmic.queue_in.put(
+            {'type': 'finish',
+             'time': time.time(),
+             'data': None
+             },
+        ),
+        Cosmic.loop
+    )"""
+
+from util.client_cosmic import Cosmic, console
+from config import ClientConfig as Config
+
+import time
+import asyncio
+from util.client_send_audio import send_audio
+from util.my_status import Status
+
+
+task = asyncio.Future()
+status = Status('开始录音', spinner='point')
+
+
+def launch_task():
+    global task
+
+    # 记录开始时间
+    t1 = time.time()
+
+    # 将开始标志放入队列
+    asyncio.run_coroutine_threadsafe(
+        Cosmic.queue_in.put({'type': 'begin', 'time': t1, 'data': None}),
+        Cosmic.loop
+    )
+
+    # 通知录音线程可以向队列放数据了
+    Cosmic.on = t1
+
+    # 打印动画：正在录音
+    status.start()
+
+    # 启动识别任务
+    task = asyncio.run_coroutine_threadsafe(
+        send_audio(),
+        Cosmic.loop,
+    )
+
+
+def cancel_task():
+    # 通知停止录音，关掉滚动条
+    Cosmic.on = False
+    status.stop()
+
+    # 取消协程任务
+    task.cancel()
+
+
+def finish_task():
+    global task
+
+    # 通知停止录音，关掉滚动条
+    Cosmic.on = False
+    status.stop()
+
+    # 通知结束任务
+    asyncio.run_coroutine_threadsafe(
+        Cosmic.queue_in.put(
+            {'type': 'finish',
+             'time': time.time(),
+             'data': None
+             },
+        ),
+        Cosmic.loop
+    )
+
+
+# ======================长按模式==================================
+
+def udp_mode(text):
+    global task
+
+    if text == 'start' and not Cosmic.on:
+        # 记录开始时间
+        launch_task()
+    elif text == 'stop':
+        # 记录持续时间，并标识录音线程停止向队列放数据
+        duration = time.time() - Cosmic.on
+
+        # 取消或停止任务
+        if duration < Config.threshold:
+            cancel_task()
+        else:
+            finish_task()
